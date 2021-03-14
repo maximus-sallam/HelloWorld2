ce50641d4eb28135c703fa7815074c4c756b094c
Maximus Sallam <maximus.sallam@gmail.com>
2021-03-14 14:31:02 +0800

    Cast double to integer and write the rounded number to the Console.

--- Program.cs
+++ Program.cs
@@ -9,35 +9,44 @@ namespace HelloWorld
         // This is where the strings go or something.
         static void Main(string[] args)
         {
-            // This is an integer
+            // These are integers.
             int num1 = 32;
             int num2 = 5;
             int sum1 = num1 + num2;
+            int myInt1;
+            int myInt2;
             
-            // This is a double.
+            // These are doubles.
             double d1 = 3.5;
             double d2 = 1.337;
             double sum2 = d1 + d2;
             
-            // This is a float.
+            // These are floats.
             float f1 = 3.5F;
             float f2 = 3.2F;
             float sum3 = f1 + f2;
             
-            // This is a string.
-            string myname = "Max";
+            // These are strings.
+            string myName = "Max";
+            string yourName;
+            
+            // Cast double to int.
+            myInt1 = (int)d2;
+            myInt2 = (int)sum2;
+            
+            Console.WriteLine(myInt1);
+            Console.WriteLine(myInt2);
             
             Console.WriteLine("The sum of " + num1 + " and " + num2 + " is " + sum1 + ".");
             Console.WriteLine("The sum of " + d1 + " and " + d2 + " is " + sum2 + ".");
             Console.WriteLine("The sum of " + f1 + " and " + f2 + " is " + sum3 + ".");
-            Console.WriteLine("My name is " + myname);
+            Console.WriteLine("My name is " + myName);
             Console.WriteLine("I am " + num1 + " years old.");
-            
             Console.Write("Enter a string and press enter: ");
             string readInput = Console.ReadLine();
             Console.WriteLine("You have entered " + readInput);
 
-            Console.Write("Enter a character and press enter: ");
+            Console.Write("Enter a character: ");
             int asciiValue = Console.Read();
             Console.WriteLine("\nASCII value is " + asciiValue);
         }
