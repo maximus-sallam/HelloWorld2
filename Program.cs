a2ee857cee785fd5ec5d477af3b17141f34209c3
Maximus Sallam <maximus.sallam@gmail.com>
2021-03-14 10:25:05 +0800

    This is a commit.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 13;
+            int num2 = 5;
+            int sum1 = num1 + num2;
+            //this is a change
+
+            double d1 = 3.5;
+            double d2 = 1.337;
+            double sum2 = d1 + d2;
+            
+            Console.WriteLine("The sum of " + num1 + " and " + num2 + " is " + sum1 + ".");
+            Console.WriteLine("The sum of " + d1 + " and " + d2 + " is " + sum2 + ".");
+            
+            Console.WriteLine(num1);
+            Console.Read();
+        }
+    }
+}
